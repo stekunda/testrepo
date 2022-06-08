cb3e9a9068d1cac25545a2f00171af3377b3685e
stekunda <99985940+stekunda@users.noreply.github.com>
2022-06-08 18:48:27 -0400

    [FEAT] Created new JS file

---
+++ firstjava.js
@@ -0,0 +1,7 @@
+// This is a new javascript file in the testrepo repository
+
+// Owner of the testrepo repository
+let userName = 'Stekunda Henry'
+
+// Displays userName on the console
+console.log(userName)
